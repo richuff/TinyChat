e470c578227309e2fe9bf79140e33b29937bb819
richuff <1691401076@qq.com>
2025-06-20 18:46:56 +0800

    chore:add test data

--- test/test_gorm.go
+++ test/test_gorm.go
@@ -1,1 +1,21 @@
 package test
+
+import "fmt"
+
+var ch chan int
+
+func main() {
+	ch = make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	for {
+		select {
+		case i := <-ch:
+			fmt.Println(i)
+		default:
+			fmt.Println("last")
+			return
+		}
+	}
+}
